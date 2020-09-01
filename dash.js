b29cb7c99e9ea1af1c44dc26fc3ba8630ee243b2
RCCodeBase <rahultdpz143@gmail.com>
2020-09-01 23:07:06 +0530

    Changes js

--- dash.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $(document).scroll(function () {
-        console.log("asda");
-      var $nav = $(".navbartop");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
